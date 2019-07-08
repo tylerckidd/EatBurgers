6d623e64a99b6bc43f92fb354867e273595acefe
tylerkidd <tylerckiddcontact@gmail.com>
2019-07-08 16:38:10 -0400

    updated js file

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -24,28 +24,3 @@ $(document).ready(function() {
 
 
 
-var dragger = document.querySelector('.dragger');
-var dragTrack = document.querySelector('.dragTrack');
-var isDragging = false, direction = 0, maxDragX = 270;
-
-var burgerBits = [".bottomBun", ".middleBurger", ".cheese", ".tomato2", ".tomato1", ".lettuce", ".topBun", ".spot1" , ".spot2" , ".spot3" ];
-
-var animIn = new TimelineMax({});
-animIn.from( ".background", 0.4, {
-  opacity: 0,
-  y:10,
-  ease:Power1.easeOut
-},)
-
-animIn.staggerFrom( burgerBits, 0.8, {
-  opacity: 0,
-  y:-100,
-  ease: Elastic.easeOut.config(1, 0.9),
-}, 0.1)
-animIn.from( ".order", 0.8, {
-  opacity: 0,
-  scale: 0.5,
-  ease:Bounce.easeOut,
-  delay: -0.5
-},)
-
