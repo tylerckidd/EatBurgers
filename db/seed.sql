dfaeead923797ebad83e090978438a6cbdd2ff67
tylerkidd <tylerckiddcontact@gmail.com>
2019-07-08 12:55:15 -0400

    updated app

---
+++ db/seed.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers(burger_name, devoured)
+	values('Bacon Mania Burger', false),
+    ('Mushroom Lover Burger', false),
+    ('Red Pepper Burger', false);
+    
+    select * from burgers;
