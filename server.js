890c5d267b155cbd3dbacc9ac3e90c037c1b4c06
tylerkidd <tylerckiddcontact@gmail.com>
2019-07-08 18:00:01 -0400

    updated server.js

--- server.js
+++ server.js
@@ -46,6 +46,6 @@ app.use(routes);
 var PORT = process.env.PORT || 3000;
 db.sequelize.sync().then(function() {
   app.listen(PORT, function() {
-    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+    console.log("App now listening on port:", PORT);
   });
 });
